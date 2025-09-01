4603bc9e5aa18521c1b371200cfd4a69f98e73f4
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2025-09-01 15:44:09 +0530

    Create lesson7.js

---
+++ Playwright JS Automation Zero to Hero/JS Fundamentals/lessons/lesson7.js
@@ -0,0 +1,18 @@
+//loops
+
+//console.log("Hello World");
+
+//for (statement1; statement2; statement3) {
+  // code block to be executed
+//}
+
+for (let i = 0; i < 5; i++) {
+  console.log("Hello World "+ i );
+}
+
+var car = ["BMW", "Volvo", "Saab", "Ford"];
+
+//for of loop
+for (let i of car) {
+  console.log(i);
+}
