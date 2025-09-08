d5cf35dfa3f5b60d677bf26954c38a01143d55d3
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2025-09-08 14:49:47 +0530

    expect

--- pw_by_Rahul_Shetty/Section_6/main.py
+++ pw_by_Rahul_Shetty/Section_6/main.py
@@ -1,4 +1,5 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
+
 
 def test_playwright_basics(playwright):
     browser = playwright.chromium.launch(headless=False)
@@ -13,8 +14,9 @@ def test_playwright_shortcut(page: Page):
 def test_core_locators(page: Page):
     page.goto("https://rahulshettyacademy.com/loginpagePractise/")
     page.get_by_label('Username:').fill('rahulshettyacademy')
-    page.get_by_label('Password:').fill('learning')
+    page.get_by_label('Password:').fill('learningjjj')
     page.get_by_role('combobox').select_option('teach')
     page.get_by_role("checkbox", name = "terms").click()
     page.get_by_role('button', name='Sign In').click()
-    page.wait_for_timeout(5000)
+    #page.wait_for_timeout(5000)
+    expect(page.get_by_text('Incorrect username/password.')).to_be_visible()
