e2db1719bf5a3eb0e4cf704aae3d939a193c83b2
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2025-09-16 22:09:51 +0530

    json

---
+++ pw_by_Rahul_Shetty/Section_10/apiBase.py
@@ -0,0 +1,24 @@
+from playwright.sync_api import Playwright
+
+orders_payload = '{"orders":[{"country":"Afghanistan","productOrderedId":"68a961459320a140fe1ca57a"}]}'
+# token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OGMwMDNhY2Y2NjlkNmNiMGFiZTY4MjgiLCJ1c2VyRW1haWwiOiJ0ZXN0QHRlc3RlZS5jb20iLCJ1c2VyTW9iaWxlIjo4NjYwMTUzNDg4LCJ1c2VyUm9sZSI6ImN1c3RvbWVyIiwiaWF0IjoxNzU3NTEwMzA0LCJleHAiOjE3ODkwNjc5MDR9.Oegp76gm-mzmR8GSesv5LgonNzYxTsbcRM2vhMP7A0c'
+class APIUtils:
+
+    def get_token(self, playwright : Playwright):
+        api_req_context = playwright.request.new_context(base_url='https://rahulshettyacademy.com')
+        response = api_req_context.post('/api/ecom/auth/login',
+                                        data = {"userEmail":"test@testee.com",
+                                        "userPassword":"ABcd@1234"})
+        assert response.ok
+        response_body = response.json()
+        return response_body['token']
+
+    def create_order(self, playwright : Playwright):
+        token = self.get_token(playwright)
+        api_req_context = playwright.request.new_context(base_url='https://rahulshettyacademy.com')
+        response = api_req_context.post('/api/ecom/order/create-order',
+                             data=orders_payload, headers={"Authorization": token,
+                             "Content-Type": "application/json"})
+
+        response_body = response.json()
+        return response_body['orders'][0]
---
+++ pw_by_Rahul_Shetty/Section_10/test_framework.py
@@ -0,0 +1,27 @@
+from playwright.sync_api import Page, expect, Playwright
+
+from apiBase import APIUtils
+
+
+def test_e2e_web_api(playwright : Playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    #create order via API
+    api_utils = APIUtils()
+    order_id = api_utils.create_order(playwright)
+
+    #login flow
+    page.goto("https://rahulshettyacademy.com/client")
+    page.get_by_placeholder("email@example.com").fill("test@testee.com")
+    page.get_by_placeholder("enter your passsword").fill("ABcd@1234")
+    page.get_by_role("button", name="Login").click()
+
+    page.locator('button').filter(has_text='Order').click()
+    page.locator('tr').filter(has_text=order_id).get_by_role('button', name='View').click()
+
+    expect(page.locator('.tagline')).to_have_text('Thank you for Shopping With Us')
+
+
+
