6d4e2152c2563625f1e471b2e65b41b3ba785cec
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2025-10-09 12:19:45 +0530

    Update main.py

--- Selenium-Python-Rahul-Shetty/Section-5/main.py
+++ Selenium-Python-Rahul-Shetty/Section-5/main.py
@@ -16,6 +16,17 @@ driver.find_element(By.NAME, "email").send_keys("email@email.com")
 driver.find_element(By.ID, "exampleInputPassword1").send_keys("123456")
 driver.find_element(By.ID, "exampleCheck1").click()
 
+#Creating xpath
+# //tagname[@attribute='value']  -> //input[@type='submit']
+
+driver.find_element(By.XPATH, "//input[@type='submit']").click()
+message = driver.find_element(By.CLASS_NAME, "alert-success").text
+print(message)
+
+#Creating CSS Selector
+# tagname[attribute='value']  -> input[type='submit']
+driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys('John')
+
 
 
 
