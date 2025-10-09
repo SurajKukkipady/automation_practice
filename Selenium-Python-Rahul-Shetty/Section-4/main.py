b6382f2498a80427a0d199b9370e7fb07d855f4e
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2025-10-09 11:32:22 +0530

    Update main.py

--- Selenium-Python-Rahul-Shetty/Section-4/main.py
+++ Selenium-Python-Rahul-Shetty/Section-4/main.py
@@ -1,8 +1,15 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.edge.service import Service
 
 #Chrome drive service
-driver = webdriver.Chrome()
+#driver = webdriver.Chrome()
+
+##Service is used to specify the path of the driver
+service = Service(r"C:\Users\suraj\Downloads\edgedriver_win64\msedgedriver.exe")
+driver = webdriver.Edge(service=service)
+
+
 driver.get("https://rahulshettyacademy.com/")
 driver.maximize_window()
 print(driver.title)
