c1895f2a1eb32469ed91d6c84f957c1bdbe4ed58
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2025-10-09 11:57:30 +0530

    Update main.py

--- Selenium-Python-Rahul-Shetty/Section-5/main.py
+++ Selenium-Python-Rahul-Shetty/Section-5/main.py
@@ -1,15 +1,20 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.service import Service
 
 #Chrome drive service
 driver = webdriver.Chrome()
 
 
-driver.get("https://rahulshettyacademy.com/")
+driver.get("https://rahulshettyacademy.com/angularpractice/")
 driver.maximize_window()
-print(driver.title)
-print(driver.current_url)
+
+# ID, xpath, CSS Selector, Name, Class Name, Link Text, Partial Link Text, Tag Name
+
+driver.find_element(By.NAME, "email").send_keys("email@email.com")
+driver.find_element(By.ID, "exampleInputPassword1").send_keys("123456")
+driver.find_element(By.ID, "exampleCheck1").click()
 
 
 
