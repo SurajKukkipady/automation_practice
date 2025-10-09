50e9dc496cb0755e9a355b1639df7d7018f72997
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2025-10-09 15:33:39 +0530

    Update demo.py

--- Selenium-Python-Rahul-Shetty/Section-5/demo.py
+++ Selenium-Python-Rahul-Shetty/Section-5/demo.py
@@ -13,6 +13,7 @@ driver.find_element(By.LINK_TEXT, 'Forgot password?').click()
 driver.find_element(By.XPATH, '//form/div[1]/input').send_keys('demo@gmail.com')
 driver.find_element(By.CSS_SELECTOR, 'form div:nth-child(2) input').send_keys('123')
 driver.find_element(By.CSS_SELECTOR, '#confirmPassword').send_keys('123')
-driver.find_element(By.XPATH, '//button[@type="submit"]').click()
+#driver.find_element(By.XPATH, '//button[@type="submit"]').click()
+driver.find_element(By.XPATH, '//button[text()="Save New Password"]').click()
 
 time.sleep(2)
