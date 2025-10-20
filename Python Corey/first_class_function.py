54c513e0f2a733584fb5c288bc482a4405d56628
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2025-10-20 13:06:14 +0530

    Update first_class_function.py

--- Python Corey/first_class_function.py
+++ Python Corey/first_class_function.py
@@ -15,3 +15,47 @@ f = square
 print(f)
 print(square)  # Output: <function square at 0x...>
 
+print(f(5))     # Output: 25
+print(square(5))  # Output: 25
+
+print('************')
+
+def square(x):
+    return x * x
+
+def cube(x):
+    return x * x * x
+
+def my_map(func, arg_list):
+    result = []
+    for i in arg_list:
+        result.append(func(i))
+    return result
+
+squares = my_map(square, [1, 2, 3, 4, 5])
+print(squares)  # Output: [1, 4, 9, 16, 25]
+
+print('************')
+
+def logger(msg):
+    def log_message():
+        print('Log:', msg)
+    return log_message
+
+log_hi = logger('Hi!')
+log_hi()  # Output: Log: Hi!
+
+print('************')
+
+def html_tag(tag):
+    def wrap_text(msg):
+        print(f'<{tag}>{msg}</{tag}>')
+    return wrap_text
+
+print_h1 = html_tag('h1')
+print_h1('Hello, World!')  # Output: <h1>Hello, World
+print_h1('Welcome to Python!')  # Output: <h1>Welcome to Python!</h1>
+
+print_p = html_tag('p')
+print_p('This is a paragraph.')  # Output: <p>This is a paragraph.</
+
