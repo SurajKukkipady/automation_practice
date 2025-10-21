f5981f572f2bf7a25054cf6722af3d5fec99dd5f
SurajKukkipady <56889466+SurajKukkipady@users.noreply.github.com>
2025-10-21 21:07:00 +0530

    Update closure.py

--- closure.py
+++ closure.py
@@ -1,11 +1,15 @@
 #closure
 
-def outer_func():
-    message = 'Hi'
+def outer_func(msg):
+    message = msg
 
     def inner_func():
         print(message)
 
-    return inner_func()
+    return inner_func
 
-outer_func()
+my_func = outer_func('hi')
+hello_func = outer_func('hello')
+
+my_func()
+hello_func()
